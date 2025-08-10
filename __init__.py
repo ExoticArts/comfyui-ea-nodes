cd93fb604d85e882f4dd33ad27310ddb303d71b8
ExoticArts <exoticarts.ai@gmail.com>
2025-08-10 03:38:59 -0700

    fix init for web extensions

--- __init__.py
+++ __init__.py
@@ -2,3 +2,8 @@
 # Single entry-point that exposes node mappings from the nodes/ package.
 
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# Tell ComfyUI where to find web extensions for this package
+WEB_DIRECTORY = "./web"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
